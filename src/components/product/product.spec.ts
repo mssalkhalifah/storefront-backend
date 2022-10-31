e396ddf8b29b4ef2d17b7066728ccc87bc93e888
mssalkhalifah <mssalkhalifah@gmail.com>
2022-10-31 20:31:54 +0300

    Added product table migration

---
+++ src/components/product/product.spec.ts
@@ -0,0 +1,7 @@
+describe('Product Model', (): void => {
+  it('should have an index method');
+  it('should have a show method');
+  it('should have a create method');
+  it('should have a method to display the top 5 most popular products');
+  it('should have a method to display products by category');
+});
